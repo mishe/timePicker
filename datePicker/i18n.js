1c681ec019555831bd3ff59c1743a2b77e69bdda
mishe <mishe@163.com>
2016-06-03 14:35:18 +0800

    Update i18n.js

--- datePicker/i18n.js
+++ datePicker/i18n.js
@@ -1,20 +1,20 @@
-module.exports={
-    'zh-cn':{
-        pickerHeader:['年','月','周','日'],
-        pickerBottom:[['月','周','日'],['周','日'],['日'],['1H','30m','15m','10m','5m']],
-        day:['日','一','二','三','四','五','六'],
-        month:['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'],
-        startDay:0,
-        btnText:['确认','取消'],
-        sizeName:'颗粒度'
+module.exports = {
+    'zh-cn': {
+        pickerHeader: ['年', '月', '周', '日'],
+        pickerBottom: [['季', '月', '周', '日'], ['周', '日'], ['日'], ['1H', '30m', '15m', '10m', '5m']],
+        day: ['日', '一', '二', '三', '四', '五', '六'],
+        month: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+        startDay: 0,
+        btnText: ['确认', '取消'],
+        sizeName: '颗粒度'
     },
-    'en':{
-        pickerHeader:['Year','Month','Week','Day'],
-        pickerBottom:[['Month','Week','Day'],['Week','Day'],['Day'],['1H','30m','15m','10m','5m']],
-        day:['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],
-        month:['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
-        startDay:0,
-        btnText:['Submit','Cancel'],
-        sizeName:'Particle'
+    'en': {
+        pickerHeader: ['Year', 'Month', 'Week', 'Day'],
+        pickerBottom: [['Quarter', 'Month', 'Week', 'Day'], ['Week', 'Day'], ['Day'], ['1H', '30m', '15m', '10m', '5m']],
+        day: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+        month: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        startDay: 0,
+        btnText: ['Submit', 'Cancel'],
+        sizeName: 'Particle'
     }
 }
